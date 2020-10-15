dc1c7ab1be8bf014a8f1a187a0e3253659b2fe06
GG <jerometonnelier@yahoo.fr>
2020-10-15 10:08:34 +0200

    Public CustomDate

--- Sources/DateExtension/Date+Ext.swift
+++ Sources/DateExtension/Date+Ext.swift
@@ -8,17 +8,17 @@
 
 import Foundation
 
-protocol DateFormatterDecodable {
+public protocol DateFormatterDecodable {
     static var dateFormatter: DateFormatter? { get }
     static var isoDateFormatter: ISO8601DateFormatter? { get }
 }
 
-struct CustomDate<E:DateFormatterDecodable>: Codable {
-    let value: Date
-    init(date: Date) {
+public struct CustomDate<E:DateFormatterDecodable>: Codable {
+    public let value: Date
+    public init(date: Date) {
         value = date
     }
-    init(from decoder: Decoder) throws {
+    public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         let text = try container.decode(String.self)
         guard let date = E.dateFormatter?.date(from: text) ?? E.isoDateFormatter?.date(from: text) else {
@@ -27,25 +27,25 @@ struct CustomDate<E:DateFormatterDecodable>: Codable {
         self.value = date
     }
     
-    func encode(to encoder: Encoder) throws {
+    public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
         guard let date = E.dateFormatter?.string(from: value) ?? E.isoDateFormatter?.string(from: value) else {
             throw CustomDateError.wrongDateFormat
         }
         try container.encode(date)
     }
-    enum CustomDateError: Error {
+    public enum CustomDateError: Error {
         case wrongDateFormat
     }
 }
 
-struct ISODateFormatterDecodable: DateFormatterDecodable {
-    static var isoDateFormatter: ISO8601DateFormatter? {
+public struct ISODateFormatterDecodable: DateFormatterDecodable {
+    public static var isoDateFormatter: ISO8601DateFormatter? {
         let dateFormatter = ISO8601DateFormatter()
         dateFormatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
         return dateFormatter
     }
-    static var dateFormatter: DateFormatter? {
+    public static var dateFormatter: DateFormatter? {
         return nil
     }
 }
