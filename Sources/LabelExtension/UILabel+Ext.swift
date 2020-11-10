5284bc64449658672f93fffd668a489ab1e96254
GG <jerometonnelier@yahoo.fr>
2020-11-10 11:17:45 +0100

    Tappable Label

--- Sources/LabelExtension/UILabel+Ext.swift
+++ Sources/LabelExtension/UILabel+Ext.swift
@@ -48,3 +48,79 @@ public extension UILabel {
     }
 }
 
+public class TappableLabel: UILabel {
+    public var tapCompletions: [String: (() -> Void)] = [:]
+    
+    public func addTapCompletion(for text: String, completion: @escaping (() -> Void)) {
+        tapCompletions[text] = completion
+    }
+    
+    public func removeTapCompletion(for text: String) {
+        tapCompletions[text] = nil
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        print("🤞 commonInit")
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(didTapOnLabel(_:))))
+        isUserInteractionEnabled = true
+    }
+    
+    @objc public func didTapOnLabel(_ gesture: UITapGestureRecognizer) {
+        print("🤞 didTapOnLabel")
+        guard let text = self.text else { return }
+        tapCompletions.forEach { tappableString, completion in
+            let range = (text as NSString).range(of: tappableString)
+            if gesture.didTapAttributedTextInLabel(label: self, inRange: range) {
+                completion()
+            }
+        }
+    }
+}
+
+public extension UITapGestureRecognizer {
+    
+    public func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
+        // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+        let layoutManager = NSLayoutManager()
+        let textContainer = NSTextContainer(size: CGSize.zero)
+        let textStorage = NSTextStorage(attributedString: label.attributedText!)
+        
+        // Configure layoutManager and textStorage
+        layoutManager.addTextContainer(textContainer)
+        textStorage.addLayoutManager(layoutManager)
+        
+        // Configure textContainer
+        textContainer.lineFragmentPadding = 0.0
+        textContainer.lineBreakMode = label.lineBreakMode
+        textContainer.maximumNumberOfLines = label.numberOfLines
+        let labelSize = label.bounds.size
+        textContainer.size = labelSize
+        
+        // Find the tapped character location and compare it to the specified range
+        let locationOfTouchInLabel = self.location(in: label)
+        let textBoundingBox = layoutManager.usedRect(for: textContainer)
+        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x,
+                                          y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y);
+        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x,
+                                                     y: locationOfTouchInLabel.y - textContainerOffset.y);
+        var indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        indexOfCharacter = indexOfCharacter + 4
+        return NSLocationInRange(indexOfCharacter, targetRange)
+    }
+}
+
