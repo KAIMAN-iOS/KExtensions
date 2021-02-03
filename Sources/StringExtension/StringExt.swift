08cb0df7c365de5ca2e7da425b3f2831c5834b8a
GG <jerometonnelier@yahoo.fr>
2021-02-03 16:25:16 +0100

    Email check

--- Sources/StringExtension/StringExt.swift
+++ Sources/StringExtension/StringExt.swift
@@ -51,7 +51,9 @@ public extension String {
         guard !self.lowercased().hasPrefix("mailto:") else { return false }
         guard let emailDetector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue) else { return false }
         let matches = emailDetector.matches(in: self, options: NSRegularExpression.MatchingOptions.anchored, range: NSRange(location: 0, length: self.count))
-        guard matches.count == 1 else { return false }
+        guard matches.count == 1,
+              let match = matches.first,
+              match.range.length == count else { return false }
         return matches[0].url?.scheme == "mailto"
     }
     
