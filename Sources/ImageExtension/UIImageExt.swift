8a4533f94d9f42d98cedd9de4830a21d0384ff51
GG <jerometonnelier@yahoo.fr>
2021-03-05 09:35:19 +0100

    resiez image

--- Sources/ImageExtension/UIImageExt.swift
+++ Sources/ImageExtension/UIImageExt.swift
@@ -17,6 +17,36 @@ public enum HEICError: Error {
 }
 
 public extension UIImage {
+    func scalePreservingAspectRatio(targetSize: CGSize) -> UIImage {
+        // Determine the scale factor that preserves aspect ratio
+        let widthRatio = targetSize.width / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        let scaleFactor = min(widthRatio, heightRatio)
+        
+        // Compute the new image size that preserves aspect ratio
+        let scaledImageSize = CGSize(
+            width: size.width * scaleFactor,
+            height: size.height * scaleFactor
+        )
+
+        // Draw and return the resized UIImage
+        let renderer = UIGraphicsImageRenderer(
+            size: scaledImageSize
+        )
+
+        let scaledImage = renderer.image { _ in
+            self.draw(in: CGRect(
+                origin: .zero,
+                size: scaledImageSize
+            ))
+        }
+        
+        return scaledImage
+    }
+}
+
+public extension UIImage {
     
     /// Returns a image that fills in newSize
     func resizedImage(newSize: CGSize) -> UIImage? {
