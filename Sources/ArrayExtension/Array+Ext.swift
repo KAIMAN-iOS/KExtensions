ab3712940554257d888233a21dc4360798e15b16
jerometonnelier <jerometonnelier@yahoo.fr>
2021-04-16 11:54:36 +0200

    sequence + uvieix addisiton

--- Sources/ArrayExtension/Array+Ext.swift
+++ Sources/ArrayExtension/Array+Ext.swift
@@ -14,3 +14,10 @@ public extension Array {
         }
     }
 }
+
+extension Sequence where Iterator.Element: Hashable {
+    func unique() -> [Iterator.Element] {
+        var seen: Set<Iterator.Element> = []
+        return filter { seen.insert($0).inserted }
+    }
+}
