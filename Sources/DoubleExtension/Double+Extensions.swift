781ec83dc044f237447abbebc9c521a4cfd12bfb
jerometonnelier <j.tonnelier@kaiman.fr>
2022-02-21 14:10:08 +0100

    update mectrics

--- Sources/DoubleExtension/Double+Extensions.swift
+++ Sources/DoubleExtension/Double+Extensions.swift
@@ -9,32 +9,76 @@
 import Foundation
 import CoreLocation
 
+extension String {
+    func bundleLocale() -> String {
+        NSLocalizedString(self, comment: "")
+    }
+}
+
+public struct ValueDisplayable<T> {
+    public let value: T
+    public let unit: String?
+}
+
 public extension Double {
+    func kilometersToLocalizedDistance(decimals: Int = 1) -> ValueDisplayable<String> {
+        return (self * 1000).metersToLocalizedDistance(decimals: decimals)
+    }
+    
+    func metersToLocalizedDistance(decimals: Int = 1) -> ValueDisplayable<String> {
+        switch (self, Locale.current.usesMetricSystem) {
+        case (let value, true) where value < 1000:
+            return ValueDisplayable<String>(value: "\(value)",
+                                            unit: "meters short".bundleLocale())
+        case (let value, true):
+            var format = "%0.\(decimals)"
+            format += decimals == 0 ? "d" : "f"
+            return ValueDisplayable<String>(value: String(format: format, value.km()),
+                                            unit: "miles short".bundleLocale())
+        case (_, false):
+            return ValueDisplayable<String>(value: "\(self.rounded(toPlaces: decimals))",
+                                            unit: "miles short".bundleLocale())
+        }
+    }
+    
+    var minutesToLocalizedTime: ValueDisplayable<String> {
+        (self * 60.0).secondsToLocalizedTime
+    }
+    
+    var secondsToLocalizedTime: ValueDisplayable<String> {
+        if self / 3600.0 > 1.0 {
+            return ValueDisplayable<String>(value: self.readableTime(), unit: nil)
+        } else {
+            return ValueDisplayable<String>(value: "\(Int(self/60.0))",
+                                            unit: "minutes short".bundleLocale())
+        }
+    }
+    
     /// Rounds the double to decimal places value
     func rounded(toPlaces places:Int) -> Double {
         let divisor = pow(10.0, Double(places))
         return (self * divisor).rounded() / divisor
     }
     
-    // Convert 1000 meters to 1km
+    /// Convert 1000 meters to 1km
     func km(_ decimals: Int = 1) -> Double {
         return (self/1000).rounded(toPlaces: decimals)
     }
     
-    // Convert a Double into localized distance String
+    /// Convert a Double into localized distance String
     func localizedDistance(addSpace: Bool = true, force: Bool = false) -> String? {
         let returnBlock: () -> String? = {
             if self > 999 {
                 if addSpace {
-                    return "\(self.km()) \(NSLocalizedString("kilometers short", comment: ""))"
+                    return "\(self.km()) \("kilometers short".bundleLocale)"
                 } else {
-                    return "\(self.km())\(NSLocalizedString("kilometers short", comment: ""))"
+                    return "\(self.km())\("kilometers short".bundleLocale)"
                 }
             } else {
                 if addSpace {
-                    return "\(Int(self.rounded(toPlaces: 0))) \(NSLocalizedString("meters short", comment: ""))"
+                    return "\(Int(self.rounded(toPlaces: 0))) \("meters short".bundleLocale)"
                 } else {
-                    return "\(Int(self.rounded(toPlaces: 0)))\(NSLocalizedString("meters short", comment: ""))"
+                    return "\(Int(self.rounded(toPlaces: 0)))\("meters short".bundleLocale)"
                 }
             }
         }
